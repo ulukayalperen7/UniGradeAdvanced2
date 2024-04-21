c594c1a62c375244130649c71c115d8fda12317b
Alperen Ulukaya <alperenulukaya07@gmail.com>
2024-04-22 00:41:40 +0300

    an example main method

--- unigrade2.java
+++ unigrade2.java
@@ -3,6 +3,35 @@ import java.util.List;
 
 public class unigrade2 {
 
+    public static void main(String[] args) {
+        Department cse = new Department("CSE", "Computer Engineering");
+        Teacher t = new Teacher("Joseph Ledet", "josephledet@akdeniz.edu.tr", 123L, cse, 1);
+        Course c101 = new Course(cse, 101, "Programming 1", "Introduction to Programming", 6, t);
+        Course c102 = new Course(cse, 102, "Programming 2", "Object Oriented Programming", 4, t);
+        Student s = new Student("Test Student", "me@somewhere.com", 123L, cse);
+        s.addCourse(c101, 80);
+        s.addCourse(c102, 30);
+        System.out.println(s.getAKTS());
+        System.out.println(s.getAttemptedAKTS());
+        System.out.println(s.getGPA());
+        System.out.println(s);
+        s = new GradStudent("Test GRADSTUDENT", "me@somewhere.com", 456L, cse, 1, "MDE");
+        s.addCourse(c101, 80);
+        s.addCourse(c102, 70);
+        System.out.println(s.getAKTS());
+        System.out.println(s.getAttemptedAKTS());
+        System.out.println(s.getGPA());
+        System.out.println(s);
+
+        cse.setChair(t);
+        Department math = new Department("MATH", "Mathematics");
+        System.out.println(cse.getCode() + " Chair = " + cse.getChair());
+        t.setDepartment(math);
+        System.out.println(cse.getCode() + " Chair = " + cse.getChair());
+        t.setDepartment(cse);
+        System.out.println(cse.getCode() + " Chair = " + cse.getChair());
+
+    }
 }
 
 class Department {
